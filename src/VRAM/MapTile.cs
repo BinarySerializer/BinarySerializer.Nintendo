5f9212bb6b4dc7ac05d88562864251d03964dbe4
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-11-02 12:29:03 +0100

    Add MapTile for screen tile entries

---
+++ src/VRAM/MapTile.cs
@@ -0,0 +1,29 @@
+﻿namespace BinarySerializer.GBA
+{
+    public class MapTile : BinarySerializable
+    {
+        public bool Pre_IsAffine { get; set; }
+
+        public int TileIndex { get; set; }
+        public bool FlipX { get; set; }
+        public bool FlipY { get; set; }
+        public int PaletteIndex { get; set; }
+
+        public override void SerializeImpl(SerializerObject s)
+        {
+            if (Pre_IsAffine)
+                TileIndex = s.Serialize<byte>((byte)TileIndex, name: nameof(TileIndex));
+            else
+                s.SerializeBitValues<ushort>(bitFunc =>
+                {
+                    TileIndex = bitFunc(TileIndex, 10, name: nameof(TileIndex));
+                    FlipX = bitFunc(FlipX ? 1 : 0, 1, name: nameof(FlipX)) == 1;
+                    FlipY = bitFunc(FlipY ? 1 : 0, 1, name: nameof(FlipY)) == 1;
+                    PaletteIndex = bitFunc(PaletteIndex, 4, name: nameof(PaletteIndex));
+                });
+        }
+
+        public override bool UseShortLog => true;
+        public override string ToString() => $"Tile(Index: {TileIndex}, FlipX: {FlipX}, FlipY: {FlipY}, Pal: {PaletteIndex})";
+    }
+}
