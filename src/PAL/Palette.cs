9271484c9b76e507d7ca944d99a79b699b0feb38
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2022-01-16 19:18:01 +0100

    Add Palette

---
+++ src/PAL/Palette.cs
@@ -0,0 +1,14 @@
+﻿namespace BinarySerializer.GBA
+{
+    public class Palette : BinarySerializable
+    {
+        public bool Pre_Is8Bit { get; set; } // True for 256 colors, otherwise 16
+
+        public RGBA555Color[] Colors { get; set; }
+
+        public override void SerializeImpl(SerializerObject s)
+        {
+            Colors = s.SerializeObjectArray<RGBA555Color>(Colors, Pre_Is8Bit ? 256 : 16, name: nameof(Colors));
+        }
+    }
+}
