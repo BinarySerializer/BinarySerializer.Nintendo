d1feea0331423bb36f80eee60f8785ea28f2b4a2
byvar <39142447+byvar@users.noreply.github.com>
2022-07-27 12:54:01 +0200

    BinarySerializer: Logger -> SystemLog

--- src/N64/RSP/Commands/RSP_Command_Placeholder.cs
+++ src/N64/RSP/Commands/RSP_Command_Placeholder.cs
@@ -4,7 +4,7 @@
     {
         public override void SerializeBits(BitSerializerObject b) 
         {
-            b.SerializerObject.LogWarning("{0}: Unparsed RSP Command: {1}", Offset, Command);
+            b.Context.SystemLog?.LogWarning("{0}: Unparsed RSP Command: {1}", Offset, Command);
             b.SerializePadding(7 * 8);
         }
     }
