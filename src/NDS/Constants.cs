94ca654d7407c201572a5514390e81588d2bb723
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2022-09-28 14:03:50 +0200

    NDS: Add constants class with screen size

---
+++ src/NDS/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace BinarySerializer.Nintendo.NDS
+{
+    public static class Constants
+    {
+        // NDS values
+        public const int ScreenWidth = 256;
+        public const int ScreenHeight = 192;
+    }
+}
