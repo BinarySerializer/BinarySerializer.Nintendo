e44ab713942d2626f11692c4b7109f5fa8eccd44
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2023-03-31 13:51:45 +0200

    Allow setting custom palette length

--- src/GBA/PAL/Palette.cs
+++ src/GBA/PAL/Palette.cs
@@ -3,12 +3,18 @@
     public class Palette : BinarySerializable
     {
         public bool Pre_Is8Bit { get; set; } // True for 256 colors, otherwise 16
+        public int Pre_CustomLength { get; set; } = -1;
 
         public RGB555Color[] Colors { get; set; }
 
         public override void SerializeImpl(SerializerObject s)
         {
-            Colors = s.SerializeObjectArray<RGB555Color>(Colors, Pre_Is8Bit ? 256 : 16, name: nameof(Colors));
+            int length = Pre_CustomLength;
+
+            if (length == -1)
+                length = Pre_Is8Bit ? 256 : 16;
+
+            Colors = s.SerializeObjectArray<RGB555Color>(Colors, length, name: nameof(Colors));
         }
     }
 }
