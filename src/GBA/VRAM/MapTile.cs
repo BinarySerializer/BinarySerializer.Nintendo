450774c96d75ca3327ef6b94fd856833c1e3365b
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2023-03-31 13:52:44 +0200

    Change MapTile to a struct for better performance

--- src/GBA/VRAM/MapTile.cs
+++ src/GBA/VRAM/MapTile.cs
@@ -1,27 +1,41 @@
 ﻿namespace BinarySerializer.Nintendo.GBA
 {
-    public class MapTile : BinarySerializable, ISerializerShortLog
+    public readonly struct MapTile : ISerializerShortLog
     {
-        public bool Pre_IsAffine { get; set; }
-
-        public int TileIndex { get; set; }
-        public bool FlipX { get; set; }
-        public bool FlipY { get; set; }
-        public int PaletteIndex { get; set; }
-
-        public override void SerializeImpl(SerializerObject s)
+        public MapTile(int tileIndex, bool flipX, bool flipY, byte paletteIndex)
         {
-            if (Pre_IsAffine)
-                TileIndex = s.Serialize<byte>((byte)TileIndex, name: nameof(TileIndex));
-            else
-                s.DoBits<ushort>(b =>
-                {
-                    TileIndex = b.SerializeBits<int>(TileIndex, 10, name: nameof(TileIndex));
-                    FlipX = b.SerializeBits<bool>(FlipX, 1, name: nameof(FlipX));
-                    FlipY = b.SerializeBits<bool>(FlipY, 1, name: nameof(FlipY));
-                    PaletteIndex = b.SerializeBits<int>(PaletteIndex, 4, name: nameof(PaletteIndex));
-                });
+            TileIndex = tileIndex;
+            FlipX = flipX;
+            FlipY = flipY;
+            PaletteIndex = paletteIndex;
         }
+
+        public int TileIndex { get; }
+        public bool FlipX { get; }
+        public bool FlipY { get; }
+        public byte PaletteIndex { get; }
+
+        public static SerializeInto<MapTile> SerializeInto_Regular = (s, x) =>
+        {
+            s.DoBits<ushort>(b =>
+            {
+                int tileIndex = b.SerializeBits<int>(x.TileIndex, 10, name: nameof(TileIndex));
+                bool flipX = b.SerializeBits<bool>(x.FlipX, 1, name: nameof(FlipX));
+                bool flipY = b.SerializeBits<bool>(x.FlipY, 1, name: nameof(FlipY));
+                byte paletteIndex = b.SerializeBits<byte>(x.PaletteIndex, 4, name: nameof(PaletteIndex));
+
+                x = new MapTile(tileIndex, flipX, flipY, paletteIndex);
+            });
+
+            return x;
+        };
+
+        public static SerializeInto<MapTile> SerializeInto_Affine = (s, x) =>
+        {
+            int tileIndex = s.Serialize<byte>((byte)x.TileIndex, name: nameof(TileIndex));
+
+            return new MapTile(tileIndex, false, false, 0);
+        };
 
         public string ShortLog => ToString();
         public override string ToString() => $"Tile(Index: {TileIndex}, FlipX: {FlipX}, FlipY: {FlipY}, Pal: {PaletteIndex})";
