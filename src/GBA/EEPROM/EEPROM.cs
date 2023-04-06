34361fbf66be99bb4f5d483f4f99d8acf30565f3
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2023-04-06 13:33:29 +0200

    Add EEPROM

---
+++ src/GBA/EEPROM/EEPROM.cs
@@ -0,0 +1,34 @@
+﻿namespace BinarySerializer.Nintendo.GBA
+{
+    public class EEPROM<T> : BinarySerializable
+        where T : BinarySerializable, new()
+    {
+        public EEPROMSize Pre_Size { get; set; } = EEPROMSize.Kbit_4;
+        public T Obj { get; set; }
+
+        public override void SerializeImpl(SerializerObject s)
+        {
+            s.DoEncoded(new EEPROMEncoder((long)Pre_Size), () =>
+            {
+                long fileOffset = s.CurrentFileOffset;
+                Obj = s.SerializeObject<T>(Obj, name: nameof(Obj));
+
+                long objSize = s.CurrentFileOffset - fileOffset;
+
+                if (objSize == (long)Pre_Size)
+                    return;
+
+                if (objSize > (long)Pre_Size)
+                    throw new BinarySerializableException(this, "EEPROM object is too big");
+
+                s.SerializePadding((long)Pre_Size - objSize);
+            });
+        }
+
+        public enum EEPROMSize
+        {
+            Kbit_4 = 0x200,
+            Kbit_64 = 0x2000,
+        }
+    }
+}
---
+++ src/GBA/EEPROM/EEPROMEncoder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace BinarySerializer.Nintendo.GBA
+{
+    public class EEPROMEncoder : IStreamEncoder
+    {
+        public EEPROMEncoder(long length)
+        {
+            Length = length;
+        }
+
+        public long Length { get; }
+        public string Name => "EEPROM";
+
+        public void DecodeStream(Stream input, Stream output)
+        {
+            const int chunkSize = 8;
+
+            byte[] buffer = new byte[chunkSize];
+
+            // The data is written reversed, 8 bytes at a time
+            for (int i = 0; i < Length; i += chunkSize)
+            {
+                int read = input.Read(buffer, 0, chunkSize);
+
+                if (read != chunkSize)
+                    throw new EndOfStreamException();
+
+                Array.Reverse(buffer);
+
+                output.Write(buffer, 0, chunkSize);
+            }
+        }
+
+        public void EncodeStream(Stream input, Stream output) => DecodeStream(input, output);
+    }
+}
