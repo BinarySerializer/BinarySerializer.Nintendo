45249bf93ed262ae56cf969b72afa8d3eb984765
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2023-04-24 15:05:27 +0200

    Improve EEPROMEncoder when encoding data

--- src/GBA/EEPROM/EEPROMEncoder.cs
+++ src/GBA/EEPROM/EEPROMEncoder.cs
@@ -13,26 +13,28 @@ namespace BinarySerializer.Nintendo.GBA
         public long Length { get; }
         public string Name => "EEPROM";
 
-        public void DecodeStream(Stream input, Stream output)
+        private void ReverseChunks(Stream input, Stream output, bool throwOnEndOfStream)
         {
             const int chunkSize = 8;
 
-            byte[] buffer = new byte[chunkSize];
+            byte[] buffer = new byte[Length];
+            int read = input.Read(buffer, 0, buffer.Length);
+
+            if (read != buffer.Length && throwOnEndOfStream)
+                throw new EndOfStreamException();
+
+            // Pad out with 0xFF
+            for (int i = read; i < buffer.Length; i++)
+                buffer[i] = 0xFF;
 
             // The data is written reversed, 8 bytes at a time
             for (int i = 0; i < Length; i += chunkSize)
-            {
-                int read = input.Read(buffer, 0, chunkSize);
+                Array.Reverse(buffer, i, chunkSize);
 
-                if (read != chunkSize)
-                    throw new EndOfStreamException();
-
-                Array.Reverse(buffer);
-
-                output.Write(buffer, 0, chunkSize);
-            }
+            output.Write(buffer, 0, buffer.Length);
         }
 
-        public void EncodeStream(Stream input, Stream output) => DecodeStream(input, output);
+        public void DecodeStream(Stream input, Stream output) => ReverseChunks(input, output, true);
+        public void EncodeStream(Stream input, Stream output) => ReverseChunks(input, output, false);
     }
 }
