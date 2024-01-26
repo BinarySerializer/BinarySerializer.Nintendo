1c152003034a07d2f5279005529af1b197d622a5
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2024-01-26 09:09:22 +0100

    Add Palette16 and Palette256

---
+++ src/GBA/PAL/Palette16.cs
@@ -0,0 +1,11 @@
+﻿namespace BinarySerializer.Nintendo.GBA
+{
+    public class Palette16 : Palette
+    {
+        public Palette16()
+        {
+            Pre_Is8Bit = false;
+            Pre_CustomLength = -1;
+        }
+    }
+}
---
+++ src/GBA/PAL/Palette256.cs
@@ -0,0 +1,11 @@
+﻿namespace BinarySerializer.Nintendo.GBA
+{
+    public class Palette256 : Palette
+    {
+        public Palette256()
+        {
+            Pre_Is8Bit = true;
+            Pre_CustomLength = -1;
+        }
+    }
+}
