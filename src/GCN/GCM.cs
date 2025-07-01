b4e5db3f1d8efd8e66fcdfe9a7a0b839d519969d
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2025-07-01 13:15:17 +0200

    Serialize GameCube ISO file system

---
+++ src/GCN/GCM.cs
@@ -0,0 +1,29 @@
+﻿namespace BinarySerializer.Nintendo.GCN
+{
+    // GameCube ISO
+    public class GCM : BinarySerializable
+    {
+        public Pointer FileSystemOffset { get; set; }
+        public uint FileSystemSize { get; set; }
+
+        public uint FileEntriesCount { get; set; }
+        public GCMFileEntry[] FileEntries { get; set; }
+
+        public override void SerializeImpl(SerializerObject s)
+        {
+            s.Goto(s.CurrentPointer + 0x424);
+            FileSystemOffset = s.SerializePointer(FileSystemOffset, name: nameof(FileSystemOffset));
+            FileSystemSize = s.Serialize<uint>(FileSystemSize, name: nameof(FileSystemSize));
+
+            s.Goto(FileSystemOffset + 0x8);
+            FileEntriesCount = s.Serialize<uint>(FileEntriesCount, name: nameof(FileEntriesCount));
+            s.Goto(FileSystemOffset);
+            Pointer fileNameTableOffset = FileSystemOffset + FileEntriesCount * 12;
+            FileEntries = s.SerializeObjectArray<GCMFileEntry>(FileEntries, FileEntriesCount, (x, i) =>
+            {
+                x.Pre_FileIndex = i;
+                x.Pre_FileNameTableOffset = fileNameTableOffset;
+            }, name: nameof(FileEntries));
+        }
+    }
+}
---
+++ src/GCN/GCMFileEntry.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace BinarySerializer.Nintendo.GCN
+{
+    public class GCMFileEntry : BinarySerializable
+    {
+        public int Pre_FileIndex { get; set; }
+        public Pointer Pre_FileNameTableOffset { get; set; }
+
+        public uint NameOffset { get; set; }
+        public bool IsDirectory { get; set; }
+    
+        // Directory
+        public uint ParentIndex { get; set; }
+        public uint NextIndex { get; set; }
+
+        // File
+        public Pointer FileOffset { get; set; }
+        public uint FileSize { get; set; }
+
+        public string Name { get; set; }
+
+        public byte[] ReadFile()
+        {
+            BinaryDeserializer s = Context.Deserializer;
+            return s.DoAt(FileOffset, () => s.SerializeArray<byte>(null, FileSize, name: Name));
+        }
+
+        public override void SerializeImpl(SerializerObject s)
+        {
+            s.DoBits<uint>(b =>
+            {
+                NameOffset = b.SerializeBits<uint>(NameOffset, 24, name: nameof(NameOffset));
+                IsDirectory = b.SerializeBits<bool>(IsDirectory, 8, name: nameof(IsDirectory));
+            });
+
+            if (IsDirectory)
+            {
+                ParentIndex = s.Serialize<uint>(ParentIndex, name: nameof(ParentIndex));
+                NextIndex = s.Serialize<uint>(NextIndex, name: nameof(NextIndex));
+            }
+            else
+            {
+                FileOffset = s.SerializePointer(FileOffset, name: nameof(FileOffset));
+                FileSize = s.Serialize<uint>(FileSize, name: nameof(FileSize));
+            }
+
+            if (Pre_FileIndex == 0)
+                Name = String.Empty;
+            else
+                s.DoAt(Pre_FileNameTableOffset + NameOffset, () => Name = s.SerializeString(Name, name: nameof(Name)));
+        }
+    }
+}
